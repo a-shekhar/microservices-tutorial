ff6fd9edab480bfa77a1d7e6ab5faa736b5c06bd
a-shekhar <rajadityashekhar@outlook.com>
2025-07-29 14:33:35 +0530

    0.3 - Connection between currency exchange and conversion

---
+++ currency-conversion-service/src/main/java/com/anjori/currency_conversion_service/CurrencyConversionController.java
@@ -0,0 +1,44 @@
+package com.anjori.currency_conversion_service;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}/quantity/{qty}")
+    public CurrencyConversion calculateCurrencyConversion(
+        @PathVariable String from,
+        @PathVariable String to,
+        @PathVariable BigDecimal qty
+    ){
+
+        HashMap<String, String> urivariables = new HashMap<>();
+        urivariables.put("from", from);
+        urivariables.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
+        ("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+         CurrencyConversion.class,
+         urivariables);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+        
+        return new CurrencyConversion().builder()
+        .id(10001L)
+        .from(from)
+        .to(to)
+        .conversionMultiple(currencyConversion.getConversionMultiple())
+        .quantity(qty)
+        .totalCalculatedAmount(qty.multiply(currencyConversion.getConversionMultiple()))
+        .environment(currencyConversion.getEnvironment())
+        .build();
+    }
+}
