365866b0ac4acd042f2ba33d06c4819e6abd6621
a-shekhar <rajadityashekhar@outlook.com>
2025-07-29 14:50:37 +0530

    0.3 - Using Fiegn

--- currency-conversion-service/src/main/java/com/anjori/currency_conversion_service/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/anjori/currency_conversion_service/CurrencyConversionController.java
@@ -3,15 +3,21 @@ package com.anjori.currency_conversion_service;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.ResponseEntity;
 
 
 @RestController
 public class CurrencyConversionController {
+
+    private final CurrencyExchangeProxy proxy;
+
+    public CurrencyConversionController(CurrencyExchangeProxy proxy){
+        this.proxy = proxy;
+    }
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}/quantity/{qty}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -38,7 +44,29 @@ public class CurrencyConversionController {
         .conversionMultiple(currencyConversion.getConversionMultiple())
         .quantity(qty)
         .totalCalculatedAmount(qty.multiply(currencyConversion.getConversionMultiple()))
-        .environment(currencyConversion.getEnvironment())
+        .environment(currencyConversion.getEnvironment() + " rest")
+        .build();
+    }
+
+    
+    @GetMapping("/currency-exchange-feign/from/{from}/to/{to}/quantity/{qty}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+        @PathVariable String from,
+        @PathVariable String to,
+        @PathVariable BigDecimal qty
+    ){
+
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        
+        return new CurrencyConversion().builder()
+        .id(10001L)
+        .from(from)
+        .to(to)
+        .conversionMultiple(currencyConversion.getConversionMultiple())
+        .quantity(qty)
+        .totalCalculatedAmount(qty.multiply(currencyConversion.getConversionMultiple()))
+        .environment(currencyConversion.getEnvironment() + " feign")
         .build();
     }
 }
