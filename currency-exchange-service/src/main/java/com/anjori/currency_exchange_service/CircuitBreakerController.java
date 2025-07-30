efc75df63cd0d07114ddb234e73724017f200457
a-shekhar <rajadityashekhar@outlook.com>
2025-07-30 14:49:50 +0530

    0.5 - Rate limiter

--- currency-exchange-service/src/main/java/com/anjori/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/anjori/currency_exchange_service/CircuitBreakerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -17,11 +18,13 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
     //@Retry(name="sample-api", fallbackMethod = "fallbackResponse")
-    @CircuitBreaker(name="default", fallbackMethod = "fallbackResponse")
+    //@CircuitBreaker(name="default", fallbackMethod = "fallbackResponse")
+    @RateLimiter(name = "default")
     public String simpleApi(){
         logger.info("Sample API call received");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
-        return forEntity.getBody();
+       // ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
+       // return forEntity.getBody();
+       return "sample-api";
     }
 
     public String fallbackResponse(Exception ex){
