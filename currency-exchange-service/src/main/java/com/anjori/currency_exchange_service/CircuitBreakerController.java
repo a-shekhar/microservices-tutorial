2df737eb8d45a607654c97d817076cfa69e98191
a-shekhar <rajadityashekhar@outlook.com>
2025-07-30 14:41:19 +0530

    0.5 - Circuit Breaker

--- currency-exchange-service/src/main/java/com/anjori/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/anjori/currency_exchange_service/CircuitBreakerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -15,7 +16,8 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(getClass().getName());
 
     @GetMapping("/sample-api")
-    @Retry(name="sample-api", fallbackMethod = "fallbackResponse")
+    //@Retry(name="sample-api", fallbackMethod = "fallbackResponse")
+    @CircuitBreaker(name="default", fallbackMethod = "fallbackResponse")
     public String simpleApi(){
         logger.info("Sample API call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
