19f8c71fe38893f84d50a292784e20976a4975e1
a-shekhar <rajadityashekhar@outlook.com>
2025-07-30 12:41:03 +0530

    0.5 - Resilience and fallback

---
+++ currency-exchange-service/src/main/java/com/anjori/currency_exchange_service/CircuitBreakerController.java
@@ -0,0 +1,28 @@
+package com.anjori.currency_exchange_service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+    @GetMapping("/sample-api")
+    @Retry(name="sample-api", fallbackMethod = "fallbackResponse")
+    public String simpleApi(){
+        logger.info("Sample API call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
+        return forEntity.getBody();
+    }
+
+    public String fallbackResponse(Exception ex){
+        return "fallback response";
+    }
+}
