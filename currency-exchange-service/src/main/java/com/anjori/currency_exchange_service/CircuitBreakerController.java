c681f73fbf3f15216c38494fd5dd0a410c10d82d
a-shekhar <rajadityashekhar@outlook.com>
2025-07-30 14:52:50 +0530

    0.5 - Bulkhead

--- currency-exchange-service/src/main/java/com/anjori/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/anjori/currency_exchange_service/CircuitBreakerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -19,7 +20,8 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
     //@Retry(name="sample-api", fallbackMethod = "fallbackResponse")
     //@CircuitBreaker(name="default", fallbackMethod = "fallbackResponse")
-    @RateLimiter(name = "default")
+    //@RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String simpleApi(){
         logger.info("Sample API call received");
        // ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
